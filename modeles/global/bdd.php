83e5fdfbda1f9cb5264729b4e7046ee9ab4cc2da
Lucas Perreau <lucasperreau@gmail.com>
2015-08-25 09:32:58 +0200

    Update bdd.php

--- modeles/global/bdd.php
+++ modeles/global/bdd.php
@@ -1,7 +1,7 @@
 <?php
 	try
 	{
-		$bdd = new PDO("mysql:host=localhost;dbname=atelier801", "root", "mcfloy");
+		$bdd = new PDO("mysql:host=localhost;dbname=atelier801", "", "");
 	}
 	catch (Exception $e)
 	{
